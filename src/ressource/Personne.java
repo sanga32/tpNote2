285d98b02cd29695d4b6f20fba9b359ba5ac89e1
Alex <Alex@Alex-PC>
2016-11-18 19:09:57 +0100

    class personne

---
+++ src/ressource/Personne.java
@@ -0,0 +1,57 @@
+package ressource;
+
+import java.util.ArrayList;
+
+public class Personne {
+	int id;
+	String nom;
+	String prenom;
+	int evaluation;
+	Personne pere;
+	ArrayList<Personne> fils;
+	
+	public Personne(){
+	}
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public int getEvaluation() {
+		return evaluation;
+	}
+	public void setEvaluation(int evaluation) {
+		this.evaluation = evaluation;
+	}
+	public Personne getPere() {
+		return pere;
+	}
+	public void setPere(Personne pere) {
+		this.pere = pere;
+	}
+	public ArrayList<Personne> getFils() {
+		return fils;
+	}
+	public void setFils(ArrayList<Personne> fils) {
+		this.fils = fils;
+	}
+	
+	
+	
+	
+}
