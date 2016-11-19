91d6b126695414cc8ab8064d4c522618356d101f
sanga32 <kev_del@hotmail.fr>
2016-11-19 14:07:06 +0100

    listener

---
+++ src/controller/ValiderLoginListener.java
@@ -0,0 +1,23 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JTextField;
+
+public class ValiderLoginListener implements ActionListener{
+
+	JTextField saisieID;
+	
+	public ValiderLoginListener(JTextField id) {
+		// TODO Auto-generated constructor stub
+		saisieID = id;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/vue/Login.java
+++ src/vue/Login.java
@@ -8,6 +8,8 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import controller.ValiderLoginListener;
+
 public class Login extends JPanel{
 
 	public Login(){
@@ -21,6 +23,7 @@ public class Login extends JPanel{
 		jl.setPreferredSize(new Dimension(50, 80));
 		id.setPreferredSize(new Dimension(120, 80));
 		valider.setPreferredSize(new Dimension(50, 50));
+		valider.addActionListener(new ValiderLoginListener(id));
 		this.add(jl, BorderLayout.CENTER);
 		this.add(id, BorderLayout.EAST);
 		this.add(valider, BorderLayout.SOUTH);
