d25de8db0bceaeeb42d052b3ad734bb9faa0a2ed
sanga32 <kev_del@hotmail.fr>
2016-11-19 14:01:25 +0100

    Interface de login

---
+++ src/vue/Login.java
@@ -0,0 +1,30 @@
+package vue;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Login extends JPanel{
+
+	public Login(){
+
+		super();
+		this.setLayout(new BorderLayout());
+		JLabel jl = new JLabel("Votre id: ");
+		JTextField id = new JTextField();
+		JButton valider = new JButton("OK");
+		
+		jl.setPreferredSize(new Dimension(50, 80));
+		id.setPreferredSize(new Dimension(120, 80));
+		valider.setPreferredSize(new Dimension(50, 50));
+		this.add(jl, BorderLayout.CENTER);
+		this.add(id, BorderLayout.EAST);
+		this.add(valider, BorderLayout.SOUTH);
+		
+
+	}
+}
